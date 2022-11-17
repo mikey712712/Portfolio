7132cb05716a29aafec5e14ca0416f7720afdbe9
Mikey71212 <michaelholton712@gmail.com>
2022-11-17 12:26:54 +1100

    quickchat description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -44,6 +44,18 @@ export default function Projects({ scrollLevel }) {
 						<Text color={"#12cc53"} fontSize={"1.7em"}>
 							QuickChat
 						</Text>
+						<Text m={[, , , "0 0 25px 0"]}>
+							<strong>Description:</strong> QuickChat is a full-stack single-page web application created over a time span of 4 days through the
+							collaboration of one of my peers and myself. It features video call and text messaging capabilities between users of the app, all
+							friend requests, online statuses, messages, and incoming/outgoing calls are updated and displayed for all current users in real
+							time. The messaging component also supports the sending of images/gifs and features a built in emoji picker.
+						</Text>
+						<Text>
+							<strong>Technologies:</strong> The frontend was created using React/Vite employing Chakra-UI for styling, with Firebase acting as
+							the backend, the backend to frontend communication functionality was written in JavaScript. The real time updating was achieved
+							using Web Real-Time Communication (WebRTC) to store peer connection info into Firebase's RealtimeDB. User authentication was
+							implemented through FirebaseAuth. All other non-sensitive data was stored in Firebase's FireStore.
+						</Text>
 					</Box>
 				</Flex>
 				<Flex p={[, , , "0 0 0 15px"]} m="0 0 65px 0" h="300px" flexFlow={["column nowrap", , "row nowrap"]}>
